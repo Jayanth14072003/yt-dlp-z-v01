75963cc94ead22e3c3390e8d46b74156abe157b6
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 15:14:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/EliteCraftStudios/UPLOADER-BOT /UPLOADER-BOT
+git clone https://github.com/Jayanth14072003/yt-dlp-z-v01
 cd /UPLOADER-BOT
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @god_luffy_ati"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by me"
 python3 bot.py
