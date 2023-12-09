7a91b457737796db5c3e1f7946ac540d6df48d46
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 17:40:12 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -14,39 +14,24 @@ import os
 import shutil
 import time
 from datetime import datetime
-
+from jlink import ptitle, pmpd, pkey
 from plugins.config import Config
 from plugins.translation import Translation
-from plugins.custom_thumbnail import *
+# from plugins.custom_thumbnail import *
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types import InputMediaPhoto
-from functions.display_progress import progress_for_pyrogram, humanbytes
-from plugins.database.database import db
+from plugins.stuff import progress_for_pyrogram, humanbytes, random_char
+# from plugins.database.database import db
 from PIL import Image
-from functions.ran_text import random_char
+# from functions.ran_text import random_char
 
-async def youtube_dl_call_back(bot, update):
-    cb_data = update.data
-    # youtube_dl extractors
-    tg_send_type, youtube_dl_format, youtube_dl_ext, ranom = cb_data.split("|")
-    print(cb_data)
+async def main_j_bot(bot, update):
+    youtube_dl_url = pmpd
     random1 = random_char(5)
-    
-    save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
-        "/" + str(update.from_user.id) + f'{ranom}' + ".json"
-    try:
-        with open(save_ytdl_json_path, "r", encoding="utf8") as f:
-            response_json = json.load(f)
-    except (FileNotFoundError) as e:
-        await bot.delete_messages(
-            chat_id=update.message.chat.id,
-            message_ids=update.message.message_id,
-            revoke=True
-        )
-        return False
-    youtube_dl_url = update.message.reply_to_message.text
-    custom_file_name = str(response_json.get("title")) + \
-        "_" + youtube_dl_format + "." + youtube_dl_ext
+    custom_vfile_name = "video" + ".mp4"
+    custom_dvfile_name = "videos" + ".mp4"
+    custom_afile_name = "audio" + ".m4a"
+    custom_dafile_name = "audios" + ".m4a"
     youtube_dl_username = None
     youtube_dl_password = None
     if "|" in youtube_dl_url:
@@ -92,26 +77,171 @@ async def youtube_dl_call_back(bot, update):
         message_id=update.message.message_id
     )
     description = Translation.CUSTOM_CAPTION_UL_FILE
-    if "fulltitle" in response_json:
-        description = response_json["fulltitle"][0:1021]
+    if description is None:
+          description = ptitle
+          #description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
     tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + f'{random1}'
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
-    download_directory = tmp_directory_for_each_user + "/" + custom_file_name
+    download_V_directory = tmp_directory_for_each_user + "/" + custom_vfile_name
+    download_DV_directory = tmp_directory_for_each_user + "/" + custom_dvfile_name
+    download_A_directory = tmp_directory_for_each_user + "/" + custom_afile_name
+    download_DA_directory = tmp_directory_for_each_user + "/" + custom_dafile_name
+    download_FV_directory = tmp_directory_for_each_user + "/" + ptitle + ".mkv"
     command_to_exec = []
-    if tg_send_type == "audio":
+    if youtube_dl_url is not None:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.DOWNLOAD_START,
+            disable_web_page_preview=True,
+            reply_to_message_id=update.message_id
+          )
+        #downloading video
         command_to_exec = [
             "yt-dlp",
-            "-c",
+            "-f", "video=600000"
             "--max-filesize", str(Config.TG_MAX_FILE_SIZE),
-            "--prefer-ffmpeg",
-            "--extract-audio",
-            "--audio-format", youtube_dl_ext,
-            "--audio-quality", youtube_dl_format,
             youtube_dl_url,
-            "-o", download_directory
+            "--allow-unplayable",
+            "-o", download_V_directory
         ]
+        if Config.HTTP_PROXY != "":
+            command_to_exec.append("--proxy")
+            command_to_exec.append(Config.HTTP_PROXY)
+        if youtube_dl_username is not None:
+            command_to_exec.append("--username")
+            command_to_exec.append(youtube_dl_username)
+        if youtube_dl_password is not None:
+            command_to_exec.append("--password")
+            command_to_exec.append(youtube_dl_password)
+        command_to_exec.append("--no-warnings")
+        # command_to_exec.append("--quiet")
+        logger.info(command_to_exec)
+        start = datetime.now()
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+        logger.info(e_response)
+        logger.info(t_response)
+        ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
+        if e_response and ad_string_to_replace in e_response:
+            error_message = e_response.replace(ad_string_to_replace, "")
+            await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=error_message
+            )
+            return False
+
+        # downloading audio
+        command_to_exec = [
+            "yt-dlp",
+            "-f", "bestaudio"
+            youtube_dl_url,
+            "--allow-unplayable",
+            "-o", download_A_directory
+        ]
+
+        await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=f"Downloading audio..!!"
+            )
+
+        command_to_exec.append("--no-warnings")
+        # command_to_exec.append("--quiet")
+        logger.info(command_to_exec)
+        # start = datetime.now()
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+
+        # Decrypting video
+        command_to_exec = [
+            'mp4decrypt',
+            '--key',pkey,
+            download_V_directory,download_DV_directory
+        ]
+        await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=f"Decrypting video..!!"
+            )
+        logger.info(command_to_exec)
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+
+        # Decrypting audio
+        command_to_exec = [
+            'mp4decrypt',
+            '--key',pkey,
+            download_A_directory,download_DA_directory
+        ]
+        await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=f"Decrypting audio..!!"
+            )
+        logger.info(command_to_exec)
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+
+        # Merging video and audio
+        command_to_exec = [
+            'ffmpeg',
+            '-i', download_DV_directory,'-i', download_DA_directory,'-vcodec' ,'copy' ,'-acodec' ,'copy',download_FV_directory
+        ]
+        await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=f"Decrypting video..!!"
+            )
+        logger.info(command_to_exec)
+        process = await asyncio.create_subprocess_exec(
+            *command_to_exec,
+            # stdout must a pipe to be accessible as process.stdout
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        # Wait for the subprocess to finish
+        stdout, stderr = await process.communicate()
+        e_response = stderr.decode().strip()
+        t_response = stdout.decode().strip()
+
+
+    # Delete the contenet(video and audio stuff)
+    # os.remove(tmp_directory_for_each_user)
     else:
         # command_to_exec = ["youtube-dl", "-f", youtube_dl_format, "--hls-prefer-ffmpeg", "--recode-video", "mp4", "-k", youtube_dl_url, "-o", download_directory]
         minus_f_format = youtube_dl_format
