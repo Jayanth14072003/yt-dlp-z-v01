13e7135f159668f543c10c7db26b0ae0bf96548d
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 21:48:34 +0530

    Update jlink.py

--- jlink.py
+++ jlink.py
@@ -21,12 +21,13 @@ import time
 import re
 
 
-def get_accesstoken():
+async def get_accesstoken():
+    import requests
     IdURL = "https://cs-jv.voot.com/clickstream/v1/get-id"
     GuestURL = "https://auth-jiocinema.voot.com/tokenservice/apis/v4/guest"
-    id = requests.get(url=IdURL).json()['id']
+    id = await requests.get(url=IdURL).json()['id']
 
-    token = requests.post(url=GuestURL, json={
+    token = await requests.post(url=GuestURL, json={
             'adId': id,
             "appName": "RJIL_JioCinema",
             "appVersion": "23.10.13.0-841c2bc7",
@@ -40,128 +41,136 @@ def get_accesstoken():
 
     return token["authToken"]
 
-def mpd_call(dlink):
-        access_token= await get_accesstoken()
-        
-        # print('\ntest link: https://www.jiocinema.com/movies/sergeant-bhojpuri/3767689\ntest link: https://www.jiocinema.com/tv-shows/kaalkoot/1/janam-din/3788001\n')
-        
-        # link = GetLink()
-        link = dlink
-        link_id = re.findall(r'.*/(.*)', link)[0].strip()
-        
-        # m3u8DL_RE = 'N_m3u8DL-RE'
-        
-        def replace_invalid_chars(title: str) -> str:
-            invalid_chars = {'<': '\u02c2', '>': '\u02c3',
-            ':': '\u02d0', '"': '\u02ba', '/': '\u2044',
-            '\\': '\u29f9', '|': '\u01c0', '?': '\u0294',
-            '*': '\u2217'}
-            
-            return ''.join(invalid_chars.get(c, c) for c in title)
-        
-        decoded = await jwt.decode(access_token, options={"verify_signature": False})
-        # print(f'\n{decoded}\n')
-        
-        deviceId = decoded['data']['deviceId']
-        uniqueid = decoded['data']['userId']
-        appName = decoded['data']['appName']
-        
-        ######
-        
-        # from requests.packages.urllib3.exceptions import InsecureRequestWarning
-        # requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-        
-        headers2 = {
-            'authority': 'apis-jiovoot.voot.com',
-            'accept': 'application/json, text/plain, */*',
-            'accesstoken': access_token,
-            'appname': appName,
-            'content-type': 'application/json',
-            'deviceid': deviceId,
-            'origin': 'https://www.jiocinema.com',
-            'referer': 'https://www.jiocinema.com/',
-            'uniqueid': uniqueid,
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-            'versioncode': '560',
-            'x-platform': 'androidweb',
-            'x-platform-token': 'web',
-        }
-        
-        json_data2 = {
-            '4k': False,
-            'ageGroup': '18+',
-            'appVersion': '3.4.0',
-            'bitrateProfile': 'xhdpi',
-            'capability': {
-                'drmCapability': {
-                    'aesSupport': 'yes',
-                    'fairPlayDrmSupport': 'yes',
-                    'playreadyDrmSupport': 'none',
-                    'widevineDRMSupport': 'yes',
-                },
-                'frameRateCapability': [
-                    {
-                        'frameRateSupport': '30fps',
-                        'videoQuality': '1440p',
-                    },
-                ],
+async def mpd_call(dlink):
+    import requests
+    #access_token = get_accesstoken()
+    #print(access_token)
+    #print(type(access_token))
+    access_token = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6IjZhYjVjMTE3LWUzNDctNGYzZS04YTU4LWQwNGU5M2ZiOGM3ZSIsInVzZXJUeXBlIjoiR1VFU1QiLCJhcHBOYW1lIjoiUkpJTF9KaW9DaW5lbWEiLCJkZXZpY2VJZCI6ImQ0Y2FkYTEzLTM4N2MtNGY5YS1hMjQzLTBmN2U4NTk3ZjA0MSIsImRldmljZVR5cGUiOiJwaG9uZSIsIm9zIjoiaW9zIiwicHJvZmlsZUlkIjoiNGJmNjY5M2EtMzgwYi00MzJhLWI0OTAtMGQ5MGQ5NDZmZjg5IiwiYWRJZCI6ImQ0Y2FkYTEzLTM4N2MtNGY5YS1hMjQzLTBmN2U4NTk3ZjA0MSIsImV4cGVyaW1lbnRLZXkiOnsiY29uZmlnS2V5IjoiNGJmNjY5M2EtMzgwYi00MzJhLWI0OTAtMGQ5MGQ5NDZmZjg5IiwiZ3JvdXBJZCI6NTMwMn0sInByb2ZpbGVEZXRhaWxzIjp7InByb2ZpbGVUeXBlIjoiYWR1bHQiLCJjb250ZW50QWdlUmF0aW5nIjoiQSJ9fSwiZXhwIjoxNzAyMTQzMzY4LCJpYXQiOjE3MDIxMzYxNjh9.ibOjSWpih0gYrSGvvLKzvpTdr4I9mFa_CvVibYRH8hffV0UvFpAdZJQEMMILZdrfAe7kQEj1NXXGzkKPuE5iTA"
+
+    # print('\ntest link: https://www.jiocinema.com/movies/sergeant-bhojpuri/3767689\ntest link: https://www.jiocinema.com/tv-shows/kaalkoot/1/janam-din/3788001\n')
+
+    # link = GetLink()
+    link = dlink
+    link_id = re.findall(r'.*/(.*)', link)[0].strip()
+
+    # m3u8DL_RE = 'N_m3u8DL-RE'
+
+    async def replace_invalid_chars(title: str) -> str:
+        invalid_chars = {'<': '\u02c2', '>': '\u02c3',
+        ':': '\u02d0', '"': '\u02ba', '/': '\u2044',
+        '\\': '\u29f9', '|': '\u01c0', '?': '\u0294',
+        '*': '\u2217'}
+
+        return ''.join(invalid_chars.get(c, c) for c in title)
+
+    decoded = jwt.decode(access_token, options={"verify_signature": False})
+    # print(f'\n{decoded}\n')
+
+    deviceId = decoded['data']['deviceId']
+    uniqueid = decoded['data']['userId']
+    appName = decoded['data']['appName']
+
+    ######
+
+    # from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    # requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    headers2 = {
+        'authority': 'apis-jiovoot.voot.com',
+        'accept': 'application/json, text/plain, */*',
+        'accesstoken': access_token,
+        'appname': appName,
+        'content-type': 'application/json',
+        'deviceid': deviceId,
+        'origin': 'https://www.jiocinema.com',
+        'referer': 'https://www.jiocinema.com/',
+        'uniqueid': uniqueid,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        'versioncode': '560',
+        'x-platform': 'androidweb',
+        'x-platform-token': 'web',
+    }
+
+    json_data2 = {
+        '4k': False,
+        'ageGroup': '18+',
+        'appVersion': '3.4.0',
+        'bitrateProfile': 'xhdpi',
+        'capability': {
+            'drmCapability': {
+                'aesSupport': 'yes',
+                'fairPlayDrmSupport': 'yes',
+                'playreadyDrmSupport': 'none',
+                'widevineDRMSupport': 'yes',
             },
-            'continueWatchingRequired': True,
-            'dolby': False,
-            'downloadRequest': False,
-            'hevc': False,
-            'kidsSafe': False,
-            'manufacturer': 'Windows',
-            'model': 'Windows',
-            'multiAudioRequired': True,
-            'osVersion': '10',
-            'parentalPinValid': True,
-        }
-        
-        response2 = await requests.post('https://apis-jiovoot.voot.com/playbackjv/v4/'+link_id+'', headers=headers2, json=json_data2, verify=False).json()
-        
-        contentType = await response2['data']['contentType']
-        
-        if contentType == 'MOVIE':
-            movie_name = await response2['data']['name']
-            title = f'{movie_name}'
-        
-        elif contentType == 'EPISODE':
-            showName = await response2['data']['show']['name']
-            season_num = await int(response2['data']['episode']['season'])
-            episode_num = await int(response2['data']['episode']['episodeNo'])
-            episode_title = await response2['data']['fullTitle']
-            
-            title = f'{showName} - S{season_num:02d}E{episode_num:02d} - {episode_title}'
-        
-        else:
-            movie_name = await response2['data']['name']
-            title = f'{movie_name}'
-        
-        title = await replace_invalid_chars(title)
-        # print(f'\n{title}\n')
-        global ptitle
-        ptitle = title
-        
-        mpd = await response2['data']['playbackUrls'][0]['url']
-        global pmpd
-        pmpd = mpd
-        
-        lic_url = await response2['data']['playbackUrls'][0]['licenseurl']
-        try:
+            'frameRateCapability': [
+                {
+                    'frameRateSupport': '30fps',
+                    'videoQuality': '1440p',
+                },
+            ],
+        },
+        'continueWatchingRequired': True,
+        'dolby': False,
+        'downloadRequest': False,
+        'hevc': False,
+        'kidsSafe': False,
+        'manufacturer': 'Windows',
+        'model': 'Windows',
+        'multiAudioRequired': True,
+        'osVersion': '10',
+        'parentalPinValid': True,
+    }
+
+    response2 = requests.post('https://apis-jiovoot.voot.com/playbackjv/v4/'+link_id+'', headers=headers2, json=json_data2, verify=False).json()
+    print(response2)
+    await asyncio.sleep(5)
+    contentType = response2['data']['contentType']
+
+    if contentType == 'MOVIE':
+        movie_name = response2['data']['name']
+        title = f'{movie_name}'
+
+    elif contentType == 'EPISODE':
+        showName = response2['data']['show']['name']
+        season_num = int(response2['data']['episode']['season'])
+        episode_num = int(response2['data']['episode']['episodeNo'])
+        episode_title = response2['data']['fullTitle']
+
+        title = f'{showName} - S{season_num:02d}E{episode_num:02d} - {episode_title}'
+
+    else:
+        movie_name = response2['data']['name']
+        title = f'{movie_name}'
+
+    title = replace_invalid_chars(title)
+    # print(f'\n{title}\n')
+    global ptitle
+    ptitle = title
+
+    mpd = response2['data']['playbackUrls'][0]['url']
+    global pmpd
+    pmpd = mpd
+
+    lic_url = response2['data']['playbackUrls'][0]['licenseurl']
+    
+    try:
             import requests
-            
+
             headers03 = {
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'Connection': 'keep-alive',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
             }
-            
-            response03 = await requests.get(mpd, headers=headers03, verify=False).text
-            
+
+            response03 = requests.get(mpd, headers=headers03, verify=False).text
+
+            await asyncio.sleep(5)
+
             pssh = re.findall(r'<cenc:pssh>(.{20,170})</cenc:pssh>', response03)[0].strip()
             # print(f'{pssh}\n')
-        
+
             headers = {
                 'authority': 'prod.media.jio.com',
                 'accesstoken': access_token,
@@ -180,8 +189,8 @@ def mpd_call(dlink):
                 'x-platform': 'Web',
                 'x-playbackid': uniqueid,
             }
-            
-            def WV_Function(pssh, lic_url, cert_b64=None):
+
+            async def WV_Function(pssh, lic_url, cert_b64=None):
                 wvdecrypt = WvDecrypt(init_data_b64=pssh, cert_data_b64=cert_b64, device=deviceconfig.device_android_generic)                   
                 widevine_license = requests.post(url=lic_url, data=wvdecrypt.get_challenge(), headers=headers, verify=False)
                 license_b64 = b64encode(widevine_license.content)
@@ -189,15 +198,15 @@ def mpd_call(dlink):
                 Correct, keyswvdecrypt = wvdecrypt.start_process()
                 if Correct:
                     return Correct, keyswvdecrypt
-            Correct, keys = await WV_Function(pssh, lic_url)
-        
+            Correct, keys = WV_Function(pssh, lic_url)
+
             global pkey
             pkey = keys[2]
             # for key in keys:
                 # print('--key ' + key)
-            
-            # ke_ys = ' '.join([f'--key {key}' for key in keys]).split()
-        except:
-            pass
 
-        return pmpd,pkey,ptitle
+            # ke_ys = ' '.join([f'--key {key}' for key in keys]).split()
+    except:
+        pass
+
+    return pmpd,pkey,ptitle
