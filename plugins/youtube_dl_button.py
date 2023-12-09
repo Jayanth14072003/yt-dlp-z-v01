1f154ea5a0af54e999e01c830bf23a846381e728
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 18:05:56 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -225,7 +225,7 @@ async def main_j_bot(bot, update):
         await bot.edit_message_text(
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id,
-                text=f"Decrypting video..!!"
+                text=f"Merging video and audio..!!"
             )
         logger.info(command_to_exec)
         process = await asyncio.create_subprocess_exec(
@@ -238,75 +238,16 @@ async def main_j_bot(bot, update):
         stdout, stderr = await process.communicate()
         e_response = stderr.decode().strip()
         t_response = stdout.decode().strip()
-
-
-    # Delete the contenet(video and audio stuff)
-    # os.remove(tmp_directory_for_each_user)
-    else:
-        # command_to_exec = ["youtube-dl", "-f", youtube_dl_format, "--hls-prefer-ffmpeg", "--recode-video", "mp4", "-k", youtube_dl_url, "-o", download_directory]
-        minus_f_format = youtube_dl_format
-        if "youtu" in youtube_dl_url:
-            minus_f_format = youtube_dl_format + "+bestaudio"
-        command_to_exec = [
-            "yt-dlp",
-            "-c",
-            "--max-filesize", str(Config.TG_MAX_FILE_SIZE),
-            "--embed-subs",
-            "-f", minus_f_format,
-            "--hls-prefer-ffmpeg", youtube_dl_url,
-            "-o", download_directory
-        ]
-    if Config.HTTP_PROXY != "":
-        command_to_exec.append("--proxy")
-        command_to_exec.append(Config.HTTP_PROXY)
-    if youtube_dl_username is not None:
-        command_to_exec.append("--username")
-        command_to_exec.append(youtube_dl_username)
-    if youtube_dl_password is not None:
-        command_to_exec.append("--password")
-        command_to_exec.append(youtube_dl_password)
-    command_to_exec.append("--no-warnings")
-    # command_to_exec.append("--quiet")
-    logger.info(command_to_exec)
-    start = datetime.now()
-    process = await asyncio.create_subprocess_exec(
-        *command_to_exec,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    logger.info(e_response)
-    logger.info(t_response)
-    ad_string_to_replace = "please report this issue on https://yt-dl.org/bug . Make sure you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call youtube-dl with the --verbose flag and include its complete output."
-    if e_response and ad_string_to_replace in e_response:
-        error_message = e_response.replace(ad_string_to_replace, "")
-        await bot.edit_message_text(
-            chat_id=update.message.chat.id,
-            message_id=update.message.message_id,
-            text=error_message
-        )
-        return False
-
-    if t_response:
-        logger.info(t_response)
-        try:
-            os.remove(save_ytdl_json_path)
-        except FileNotFoundError as exc:
-            pass
-        
+      
         end_one = datetime.now()
         time_taken_for_download = (end_one -start).seconds
         file_size = Config.TG_MAX_FILE_SIZE + 1
         try:
-            file_size = os.stat(download_directory).st_size
+            file_size = os.stat(download_FV_directory).st_size
         except FileNotFoundError as exc:
-            download_directory = os.path.splitext(download_directory)[0] + "." + "mkv"
+            download_FV_directory = os.path.splitext(download_FV_directory)[0] + "." + "mkv"
             # https://stackoverflow.com/a/678242/4723940
-            file_size = os.stat(download_directory).st_size
+            file_size = os.stat(download_FV_directory).st_size
         if file_size > Config.TG_MAX_FILE_SIZE:
             await bot.edit_message_text(
                 chat_id=update.message.chat.id,
@@ -325,85 +266,59 @@ async def main_j_bot(bot, update):
             )
             logger.info(images)'''
             await bot.edit_message_text(
-                text=Translation.UPLOAD_START,
+                text=f"Download completed.",
                 chat_id=update.message.chat.id,
                 message_id=update.message.message_id
             )
 
             # ref: message from @Sources_codes
             start_time = time.time()
-            if (await db.get_upload_as_doc(update.from_user.id)) is False:
-                thumbnail = await Gthumb01(bot, update)
-                await bot.send_document(
-                    chat_id=update.message.chat.id,
-                    document=download_directory,
-                    thumb=thumbnail,
-                    caption=description,
-                    reply_to_message_id=update.message.reply_to_message.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
+            if os.path.exists(download_FV_directory):
+                try:
+                    thumbnail = await Gthumb(bot, update)
+                    await bot.send_document(
+                        chat_id=update.message.chat.id,
+                        document=download_FV_directory,
+                        thumb=thumbnail,
+                        caption=description,
+                        reply_to_message_id=update.message.reply_to_message.message_id,
+                        progress=progress_for_pyrogram,
+                        progress_args=(
+                            "Uploading Document..!",
+                            update.message,
+                            start_time
+                        )
                     )
-                )
-            else:
-                 width, height, duration = await Mdata01(download_directory)
-                 thumb_image_path = await Gthumb02(bot, update, duration, download_directory)
-                 await bot.send_video(
-                    chat_id=update.message.chat.id,
-                    video=download_directory,
-                    caption=description,
-                    duration=duration,
-                    width=width,
-                    height=height,
-                    supports_streaming=True,
-                    thumb=thumb_image_path,
-                    reply_to_message_id=update.message.reply_to_message.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
+                except:
+                     width, height, duration = await Mdata01(download_directory)
+                     thumb_image_path = await Gthumb(bot, update, duration, download_directory)
+                     await bot.send_video(
+                        chat_id=update.message.chat.id,
+                        video=download_FV_directory,
+                        caption=description,
+                        duration=duration,
+                        width=width,
+                        height=height,
+                        supports_streaming=True,
+                        thumb=thumb_image_path,
+                        reply_to_message_id=update.message.reply_to_message.message_id,
+                        progress=progress_for_pyrogram,
+                        progress_args=(
+                            "Uploading Video",
+                            update.message,
+                            start_time
+                        )
                     )
-                )
-            if tg_send_type == "audio":
-                duration = await Mdata03(download_directory)
-                thumbnail = await Gthumb01(bot, update)
-                await bot.send_audio(
-                    chat_id=update.message.chat.id,
-                    audio=download_directory,
-                    caption=description,
-                    parse_mode="HTML",
-                    duration=duration,
-                    thumb=thumbnail,
-                    reply_to_message_id=update.message.reply_to_message.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
-                )
-            elif tg_send_type == "vm":
-                width, duration = await Mdata02(download_directory)
-                thumbnail = await Gthumb02(bot, update, duration, download_directory)
-                await bot.send_video_note(
-                    chat_id=update.message.chat.id,
-                    video_note=download_directory,
-                    duration=duration,
-                    length=width,
-                    thumb=thumbnail,
-                    reply_to_message_id=update.message.reply_to_message.message_id,
-                    progress=progress_for_pyrogram,
-                    progress_args=(
-                        Translation.UPLOAD_START,
-                        update.message,
-                        start_time
-                    )
-                )
+            
             else:
                 logger.info("Did this happen? :\\")
+                await bot.send_message(
+                text=f"Downloaded file not found..!",
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                disable_web_page_preview=True
+                )
+              
             end_two = datetime.now()
             time_taken_for_upload = (end_two - end_one).seconds
             try:
@@ -417,3 +332,20 @@ async def main_j_bot(bot, update):
                 message_id=update.message.message_id,
                 disable_web_page_preview=True
             )
+    # Delete the contenet(video and audio stuff)
+    # os.remove(tmp_directory_for_each_user)
+    else:
+        # command_to_exec = ["youtube-dl", "-f", youtube_dl_format, "--hls-prefer-ffmpeg", "--recode-video", "mp4", "-k", youtube_dl_url, "-o", download_directory]
+        await bot.edit_message_text(
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id,
+                text=f"There is problem with your link please send valid link..!!"
+            )
+
+    if t_response:
+        logger.info(t_response)
+        try:
+            os.remove(save_ytdl_json_path)
+        except FileNotFoundError as exc:
+            pass
+        
