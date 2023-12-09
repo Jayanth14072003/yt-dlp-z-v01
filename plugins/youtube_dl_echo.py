52efbbec25137129e7e6fa1c703dbf5418e97ad8
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 16:00:19 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -1,1 +1,20 @@
+# KA18 the @legend580
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+import requests, urllib.parse, filetype, os, time, shutil, tldextract, asyncio, json, math
+from PIL import Image
+from plugins.config import Config
+import time
+from plugins.translation import Translation
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import filters
+from pyrogram import Client
+from plugins.stuff import progress_for_pyrogram, humanbytes, TimeFormatter, random_char
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import Thumbnail
