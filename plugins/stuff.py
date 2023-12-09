b84ce093d2a91e39b5357671da0607d0a778be0f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-12-09 15:37:22 +0530

    Create stuff.py

---
+++ plugins/stuff.py
@@ -0,0 +1,99 @@
+# KA18 the legend
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+import asyncio
+import os
+import time
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+import random
+import numpy
+from PIL import Image
+
+
+
+async def take_screen_shot(video_file, output_directory, ttl):
+    # https://stackoverflow.com/a/13891070/4723940
+    out_put_file_name = output_directory + \
+        "/" + str(time.time()) + ".jpg"
+    file_genertor_command = [
+        "ffmpeg",
+        "-ss",
+        str(ttl),
+        "-i",
+        video_file,
+        "-vframes",
+        "1",
+        out_put_file_name
+    ]
+    # width = "90"
+    process = await asyncio.create_subprocess_exec(
+        *file_genertor_command,
+        # stdout must a pipe to be accessible as process.stdout
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+    e_response = stderr.decode().strip()
+    t_response = stdout.decode().strip()
+    if os.path.lexists(out_put_file_name):
+        return out_put_file_name
+    else:
+        return None
+
+
+
+async def Gthumb(bot, update, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    # db_thumbnail = await db.get_thumbnail(update.from_user.id)
+    # if db_thumbnail is not None:
+        # thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+    # else:
+    thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
+
+    return thumbnail
+
+async def Mdata01(download_directory): #here we are extracting the video information such as duration,height and width
+
+          width = 0
+          height = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+              if metadata.has("height"):
+                  height = metadata.get("height")
+
+          return width, height, duration
+
+async def Mdata02(download_directory): #here we are extracting the video-note information such as duration and width
+
+          width = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+
+          return width, duration
+
+async def Mdata03(download_directory): #here we are extracting the audeo information such as duration
+
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+
+          return duration
